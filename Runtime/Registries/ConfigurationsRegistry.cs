522ea93d88507ba564fb09e263b51984da41302c
Andrzej Więckowski <andywiecko@gmail.com>
2022-06-08 18:31:35 +0200

    refactor: configurations registry simplification

--- Runtime/Registries/ConfigurationsRegistry.cs
+++ Runtime/Registries/ConfigurationsRegistry.cs
@@ -1,42 +1,28 @@
 using System;
 using System.Collections.Generic;
-using System.Linq;
 
 namespace andywiecko.ECS
 {
     public class ConfigurationsRegistry
     {
-        static ConfigurationsRegistry()
-        {
-            foreach (var assembly in AppDomain.CurrentDomain.GetAssemblies())
-            {
-                foreach (var type in assembly.GetTypes())
-                {
-                    if (typeof(IConfiguration).IsAssignableFrom(type) && !type.IsAbstract)
-                    {
-                        staticCachedConfigurations.Add(type, default);
-                    }
-                }
-            }
-        }
-
-        private static readonly Dictionary<Type, IConfiguration> staticCachedConfigurations = new();
-
         public event Action OnRegistryChange;
-        private readonly Dictionary<Type, IConfiguration> configs;
-
-        public ConfigurationsRegistry()
-        {
-            configs = staticCachedConfigurations.ToDictionary(i => i.Key, i => default(IConfiguration));
-        }
+        private readonly Dictionary<Type, IConfiguration> configs = new();
 
         public T Get<T>() where T : class, IConfiguration => configs[typeof(T)] as T;
 
-        public void Set<T>(T config) where T : IConfiguration
+        public void Set<T>(T config) where T : class, IConfiguration
         {
-            //TODO Add warning if system is already present
-            configs[typeof(T)] = config;
-            OnRegistryChange?.Invoke();
+            var t = typeof(T);
+
+            if (configs.ContainsKey(t) && config != null)
+            {
+                throw new ArgumentException($"Configuration of type {t} is already set in the registry!");
+            }
+            else
+            {
+                configs[typeof(T)] = config;
+                OnRegistryChange?.Invoke();
+            }
         }
     }
 }
