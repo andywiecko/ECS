820973ee3dca66040c22f2a7481f39ba6372fbcb
Andrzej Więckowski <andywiecko@gmail.com>
2022-06-14 19:14:33 +0200

    refactor: components registry
    Introduced a few different constructors for components registry which enable for setting the types for which mappings should be generated.

--- Runtime/Base/World.cs
+++ Runtime/Base/World.cs
@@ -1,3 +1,6 @@
+using Newtonsoft.Json.Linq;
+using System.Linq;
+using System.Reflection;
 using UnityEngine;
 
 namespace andywiecko.ECS
@@ -6,7 +9,23 @@ namespace andywiecko.ECS
     {
         //public IdCounter<IEntity> EntityCounter { get; } = new(); // TODO: move to EntitesRegistry
         public ConfigurationsRegistry ConfigurationsRegistry { get; } = new();
-        public ComponentsRegistry ComponentsRegistry { get; } = new();
+        public ComponentsRegistry ComponentsRegistry => componentsRegistry ??= new(assembliesNames.Select(i => Assembly.Load(i)));
+        private ComponentsRegistry componentsRegistry;
         public SystemsRegistry SystemsRegistry { get; } = new();
+
+#if UNITY_EDITOR
+        [SerializeField]
+        private UnityEditorInternal.AssemblyDefinitionAsset[] targetAssemblies = { };
+#endif
+
+        [SerializeField, HideInInspector]
+        private string[] assembliesNames = { };
+
+        private void OnValidate()
+        {
+            assembliesNames = targetAssemblies?
+                .Where(i => i != null)
+                .Select(i => JObject.Parse(i.text)["name"].ToString()).ToArray();
+        }
     }
 }
--- Runtime/Registries/ComponentsRegistry.cs
+++ Runtime/Registries/ComponentsRegistry.cs
@@ -8,75 +8,24 @@ namespace andywiecko.ECS
 {
     public class ComponentsRegistry
     {
-        #region Statics
-        private static readonly Dictionary<Type, IReadOnlyList<Type>> derivedTypesToInterfaces = new();
-        private static readonly HashSet<Type> staticComponentInterfaces = new();
-        private static readonly List<(Type t, Type t1, Type t2)> tupleTypes = new();
-
-        static ComponentsRegistry()
-        {
-            foreach (var assembly in AppDomain.CurrentDomain.GetAssemblies())
-            {
-#if UNITY_EDITOR
-                foreach (var type in assembly.GetTypes())
-#else
-                foreach (var type in assembly
-                    .GetTypes()
-                    .Where(i => i.GetCustomAttributes<FakeAttribute>().Count() == 0))
-#endif
-                {
-                    RegisterTypeInterfaces(type);
-                }
-            }
-
-            RegisterTupleTypes();
-        }
-
-        private static void RegisterTypeInterfaces(Type type)
-        {
-            var list = new List<Type>();
-            foreach (var @interface in type.GetInterfaces())
-            {
-                if (typeof(IComponent).IsAssignableFrom(@interface) &&
-                    @interface.ContainsGenericParameters == false)
-                {
-                    list.Add(@interface);
-                    staticComponentInterfaces.Add(@interface);
-                }
-            }
-            derivedTypesToInterfaces.TryAdd(type, list);
-        }
-
-        private static void RegisterTupleTypes()
-        {
-            foreach (var assembly in AppDomain.CurrentDomain.GetAssemblies())
-            {
-                foreach (var type in assembly.GetTypes())
-                {
-                    if (typeof(ComponentsTuple).IsAssignableFrom(type) && type.IsAbstract == false)
-                    {
-                        var baseType = type.BaseType;
-                        var args = baseType.GetGenericArguments();
-                        var tuple = (type, args[0], args[1]);
-                        tupleTypes.Add(tuple);
-                    }
-                }
-            }
-        }
-        #endregion
-
         public IdCounter<IComponent> Counter { get; } = new();
 
         private readonly Dictionary<Type, IList> components = new();
         private readonly Dictionary<Type, Action<object>> onAddActions = new();
         private readonly Dictionary<Type, Action<object>> onRemoveActions = new();
+        private readonly IReadOnlyDictionary<Type, IReadOnlyList<Type>> typeToInterfaces;
 
-        public ComponentsRegistry()
+        public ComponentsRegistry(IEnumerable<Type> types)
         {
-            components = staticComponentInterfaces.ToDictionary(i => i, i => CreateListOf(i));
-            onAddActions = staticComponentInterfaces.ToDictionary(i => i, i => default(Action<object>));
-            onRemoveActions = staticComponentInterfaces.ToDictionary(i => i, i => default(Action<object>));
+            typeToInterfaces = GetTypeInterfacesMapping(types)
+                .ToDictionary(i => i.type, i => i.interfaces);
 
+            var componentInterfaces = typeToInterfaces.Values.SelectMany(i => i).Distinct();
+            components = componentInterfaces.ToDictionary(i => i, i => CreateListOf(i));
+            onAddActions = componentInterfaces.ToDictionary(i => i, i => default(Action<object>));
+            onRemoveActions = componentInterfaces.ToDictionary(i => i, i => default(Action<object>));
+
+            var tupleTypes = GetTuple2Types(types);
             foreach (var (t, t1, t2) in tupleTypes)
             {
                 SubscribeOnAdd(t1, (object i) => OnAddComponentItem1(i, t, t2));
@@ -84,7 +33,25 @@ namespace andywiecko.ECS
             }
 
             static IList CreateListOf(Type t) => Activator.CreateInstance(typeof(List<>).MakeGenericType(t)) as IList;
+
+            static IEnumerable<(Type type, IReadOnlyList<Type> interfaces)> GetTypeInterfacesMapping(IEnumerable<Type> types) =>
+                types.Select(t => (t, t
+                    .GetInterfaces()
+                    .Where(i => typeof(IComponent).IsAssignableFrom(i) && i.ContainsGenericParameters == false)
+                    .ToArray() as IReadOnlyList<Type>)
+            );
+
+            static IEnumerable<(Type t, Type t1, Type t2)> GetTuple2Types(IEnumerable<Type> types) =>
+                types.Where(i => typeof(ComponentsTuple).IsAssignableFrom(i) && i.IsAbstract == false)
+                    .Select(i =>
+                    {
+                        var args = i.BaseType.GetGenericArguments();
+                        return (i, args[0], args[1]);
+                    });
         }
+
+        public ComponentsRegistry(IEnumerable<Assembly> assemblies) : this(types: assemblies.SelectMany(i => i.GetTypes())) { }
+        public ComponentsRegistry() : this(assemblies: AppDomain.CurrentDomain.GetAssemblies()) { }
 
         public IReadOnlyList<T> GetComponents<T>() where T : IComponent => components[typeof(T)] as List<T>;
         public IEnumerable GetComponents(Type type) => components[type];
@@ -92,7 +59,7 @@ namespace andywiecko.ECS
         public void Add<T>(T instance) where T : IComponent
         {
             var type = instance.GetType();
-            foreach (var @interface in derivedTypesToInterfaces[type])
+            foreach (var @interface in typeToInterfaces[type])
             {
                 components[@interface].Add(instance);
                 onAddActions[@interface]?.Invoke(instance);
@@ -102,7 +69,7 @@ namespace andywiecko.ECS
         public void Remove<T>(T instance) where T : IComponent
         {
             var type = instance.GetType();
-            foreach (var @interface in derivedTypesToInterfaces[type])
+            foreach (var @interface in typeToInterfaces[type])
             {
                 components[@interface].Remove(instance);
                 onRemoveActions[@interface]?.Invoke(instance);
